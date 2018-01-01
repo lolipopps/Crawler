b1cb7adf20075fe0044b1e7e62495c586dd528ef
lolipopps <hytmail@whu.edu.cn>
2018-01-01 15:10:26 +0800

    基础知识

---
+++ iteration.py
@@ -0,0 +1,26 @@
+# -*- coding: utf8 -*-
+import itertools
+from Crawler import get_html
+
+
+def iteration():
+    max_errors = 5 # maximum number of consecutive download errors allowed
+    num_errors = 0 # current number of consecutive download errors
+    for page in itertools.count(1):
+        url = 'http://example.webscraping.com/view/-{}'.format(page)
+        html = get_html.download(url)
+        if html is None:
+            # received an error trying to download this webpage
+            num_errors += 1
+            if num_errors == max_errors:
+                # reached maximum amount of errors in a row so exit
+                break
+            # so assume have reached the last country ID and can stop downloading
+        else:
+            # success - can scrape the result
+            # ...
+            num_errors = 0
+
+
+if __name__ == '__main__':
+    iteration()
